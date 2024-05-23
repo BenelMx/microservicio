a39c6fac66d43912a45c5221bf2ac7e97362c0fa
BenelMx <132118364+BenelMx@users.noreply.github.com>
2024-05-22 22:38:56 -0600

    Add files via upload

---
+++ Frontend/script.js
@@ -0,0 +1,82 @@
+/* eslint-disable prettier/prettier */
+document
+  .getElementById('newUserForm')
+  .addEventListener('submit', async function (event) {
+    event.preventDefault(); 
+
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const nameError = document.getElementById('name-error');
+    const emailError = document.getElementById('email-error');
+
+    if (!nameInput.validity.valid || !emailInput.validity.valid) {
+      showError(
+        'Por favor complete correctamente todos los campos.',
+        '#name',
+        nameError,
+      );
+      showError(
+        'Por favor complete correctamente todos los campos.',
+        '#email',
+        emailError,
+      );
+      return;
+    }
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+
+    const newUser = { name, email };
+
+    try {
+      const response = await fetch('http://localhost:3001/examen', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      if (!response.ok) {
+        throw new Error('El usuario ya existe en la base de datos');
+      }
+
+      const data = await response.json();
+      showMessage('success', `Usuario ${data.name} creado con éxito`);
+      localStorage.setItem('userData', JSON.stringify(data)); 
+    } catch (error) {
+      showMessage('error', `Error: ${error.message}`);
+    }
+  });
+
+document.getElementById('enterButton').addEventListener('click', function () {
+  const userData = localStorage.getItem('userData');
+  if (userData) {
+    window.location.href = 'ver_datos.html';
+  } else {
+    showMessage('error', 'No hay datos de usuario disponibles.');
+  }
+});
+
+function showError(message, inputId, errorElement) {
+  errorElement.textContent = message;
+  errorElement.className = 'error-message active';
+  document.querySelector(inputId).classList.add('error-input');
+
+  setTimeout(function () {
+    errorElement.textContent = '';
+    errorElement.className = 'error-message';
+    document.querySelector(inputId).classList.remove('error-input');
+  }, 5000); 
+}
+
+function showMessage(type, message) {
+  const responseDiv = document.getElementById('response');
+  responseDiv.innerText = message;
+  responseDiv.className = type;
+  responseDiv.style.opacity = '1';
+
+  setTimeout(function () {
+    responseDiv.style.opacity = '0';
+  }, 5000); 
+}
---
+++ Frontend/server.js
@@ -0,0 +1,77 @@
+/* eslint-disable prettier/prettier */
+document.addEventListener('DOMContentLoaded', function () {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    if (userData) {
+        const userDataDiv = document.getElementById('userData');
+        userDataDiv.innerHTML = `<p>Nombre: ${userData.name}</p><p>Email: ${userData.email}</p>`;
+    } else {
+        document.getElementById('userData').innerText = 'No hay datos de usuario disponibles.';
+    }
+
+    document.getElementById('showProductsBtn').addEventListener('click', async function () {
+        try {
+            const response = await fetch('http://localhost:3001/products');
+            if (!response.ok) {
+                throw new Error('Error al obtener la lista de productos');
+            }
+
+            const products = await response.json();
+            const productTableBody = document.querySelector('#productTable tbody');
+            productTableBody.innerHTML = ''; 
+
+            products.forEach(product => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${product.id}</td>
+                    <td>${product.name}</td>
+                    <td>${product.price}</td>
+                    <td>${product.amount}</td>
+                `;
+                productTableBody.appendChild(row);
+            });
+        } catch (error) {
+            console.error('Error:', error);
+            const productTableBody = document.querySelector('#productTable tbody');
+            productTableBody.innerHTML = '<tr><td colspan="4">No se pudieron cargar los productos</td></tr>';
+        }
+    });
+
+    document.getElementById('addProductBtn').addEventListener('click', function () {
+        const form = document.getElementById('addProductForm');
+        form.style.display = form.style.display === 'none' ? 'block' : 'none';
+    });
+
+    document.getElementById('productForm').addEventListener('submit', async function (event) {
+        event.preventDefault();
+
+        const productName = document.getElementById('productName').value;
+        const productPrice = parseFloat(document.getElementById('productPrice').value);
+        const productQuantity = parseInt(document.getElementById('productQuantity').value);
+
+        try {
+            const response = await fetch('http://localhost:3001/products', { 
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: productName, price: productPrice, amount: productQuantity })
+            });
+
+            if (!response.ok) {
+                throw new Error('Error al agregar el producto');
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+            document.getElementById('showProductsBtn').click();
+
+            document.getElementById('productForm').reset();
+
+            document.getElementById('addProductForm').style.display = 'none';
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
+});
+
